6952e3eb0351dd043da01b05b65a18bba5418c9a
joaoSilva240 <joao.silvaoliver240@gmail.com>
2025-05-05 16:26:07 -0300

    chore: Atualização do clima em tempo real concertado

--- src/App.tsx
+++ src/App.tsx
@@ -4,79 +4,85 @@ import './App.css'
 
 export default function App() {
 
-  const [dados, setDados] = useState<{ nomeCidade: string;temperatura:number[];dataDeHoje:string } | null>(null)
-  const [bgColor, setBgColor] = useState("#FFe142")
-  const [cidade, setCidade] = useState("Brasil")
-  const [coordenadas, setCoordenadas] = useState<{ latitude: string; longitude: string } | null>(null)
-
-  useEffect(() => {
-    const buscaPelaCidade = async () => {
-      try {
-        const response = await axios.get('https://nominatim.openstreetmap.org/search', {
-          params: {
-            q: cidade,
-            format: 'json',
-            limit: 1,
-          }
-        }
-        )
-        if (response.data.length > 0) {
-          const { lat, lon } = response.data[0];
-          setCoordenadas({ latitude: lat, longitude: lon })
-        }
+  const [dados, setDados] = useState<{ nomeCidade: string; temperatura: number; dataDeHoje: string; horaAtual: string } | null>(null)
+  const [cidade, setCidade] = useState("")
+  const [horaAtual, setHoraAtual] = useState("")
+  const [temperaturaAtual, setTemperaturaAtual] = useState<number | null>(null)
 
 
-      } catch (error) {
-        console.error('não foi possivel encontrar')
-      }
-    }
-    buscaPelaCidade()
-  }
-    , [cidade])
+  const [climaAtual, setClimaAtual] = useState({ Clima: "Ensolarado", bgColor: "#FFe142" })
 
-  useEffect(() => {
-    if (!coordenadas) return
-    
-    const buscarClima = async () => {
-      const response = await axios.get('https://api.open-meteo.com/v1/forecast', {
+  async function handleNewWeather() {
+    try {
+      const response = await axios.get('https://nominatim.openstreetmap.org/search', {
         params: {
-          latitude: coordenadas?.latitude,
-          longitude: coordenadas?.longitude,
-          hourly: 'temperature_2m',
-          timezone: 'auto',
+          q: cidade,
+          format: 'json',
+          limit: 1,
         }
-      })
-      if (
-        response.data &&
-        response.data.hourly &&
-        response.data.hourly.temperature_2m
-      ) {
-        setDados({
-          nomeCidade: cidade,
-          temperatura: response.data.hourly.temperature_2m.slice(0, 12),
-          dataDeHoje:response.data.hourly.time.slice(0,12),
-        });
-      } else {
-        console.error("Dados de temperatura não encontrados na resposta:", response.data);
       }
-
-      console.log(response)
+      )
+      if (response.data.length > 0) {
+        const { lat, lon } = response.data[0];
+        buscarClima(lat, lon)
+      }
+    } catch (error) {
+      console.error('não foi possivel encontrar')
     }
-    buscarClima()
-  }, [coordenadas, cidade])
+  }
 
 
 
+  async function buscarClima(latitude?: string, longitude?: string) {
+    const now = new Date()
+    const currentHourISO = now.toISOString().slice(0, 13)
+    const pegarHoraAtual = new Date();
+    setHoraAtual(pegarHoraAtual.toLocaleTimeString('pt-BR'));
+
+    const response = await axios.get('https://api.open-meteo.com/v1/forecast', {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+        hourly: 'temperature_2m',
+        timezone: 'auto',
+      }
+    })
+    if (
+      response.data &&
+      response.data.hourly &&
+      response.data.hourly.temperature_2m
+    ) {
+
+      const timeArray: string[] = response.data.hourly.time
+      const temperatureArray: number[] = response.data.hourly.temperature_2m
+
+
+      const horaIndex = timeArray.findIndex(time => time.startsWith(currentHourISO))
+      const tempAtual = horaIndex !== -1 ? temperatureArray[horaIndex] : null
+
+      setTemperaturaAtual(tempAtual)
+
+      setDados({
+        nomeCidade: cidade,
+        temperatura: tempAtual ?? 0,
+        dataDeHoje: new Date(timeArray[0]).toLocaleDateString('pt-BR'),
+        horaAtual: horaAtual,
+      })
+    }
+  }
 
   return (
-    <div className='p-20 w-screen h-screen flex flex-col items-center justify-center gap-[5vh] ' style={{ backgroundColor: bgColor }}>
-      <button className='p-5 bg-black text-white rounded-xl' type="submit">clique aqui</button>
+    <div className='p-20 w-screen h-screen flex flex-col items-center justify-center gap-[5vh] ' style={{ backgroundColor: climaAtual.bgColor }}>
       <input onChange={(e) => setCidade(e.target.value)} className='p-3 rounded-4xl text-xl text-center bg-white' placeholder='cidade/país ' />
-      <div className='pl-20 pr-20 bg-black text-white rounded-xl'>{dados?.dataDeHoje[0]} </div>
-      <div className='m-auto text-7xl'>{dados?.temperatura[0]} C° </div>
-      <div>clima do dia </div>
-     
-   
+      <div className='pl-20 pr-20 bg-black text-white rounded-xl'>{dados?.dataDeHoje} </div>
+      <div>{climaAtual.Clima}</div>
+      <div className='m-auto text-8xl'>{dados?.temperatura}C° </div>
+      <div>{horaAtual}</div>
+      <button onClick={() => handleNewWeather()} className='p-3 bg-black rounded-4xl' style={{ color: climaAtual.bgColor }} type="submit">clique aqui</button>
+
+
     </div>
   )
 }
+
+
